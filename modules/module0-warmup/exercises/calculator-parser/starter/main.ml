65a254a28a2d3f4afcba27dc0808ab1d9b4d0bda
Flavia Daniels <danielsflavia@gmail.com>
2026-02-28 16:43:24 -0500

    Complete Exercise 5: Calculator Parser

--- modules/module0-warmup/exercises/calculator-parser/starter/main.ml
+++ modules/module0-warmup/exercises/calculator-parser/starter/main.ml
@@ -1,64 +1,47 @@
 (* ================================================================
    Exercise 5: Calculator Parser -- Main Driver
-   ================================================================
-
-   This file reads expressions from built-in examples, parses them
-   with the Menhir-generated parser, and prints + evaluates the
-   resulting AST.
-
-   TODO: Implement [string_of_op], [string_of_expr], and [eval].
-   The parse + main driver code is provided.
-
-   Run with:  dune exec modules/module0-warmup/exercises/calculator-parser/starter/main.exe
    ================================================================ *)
 
 open Ast
 
-(* ----------------------------------------------------------------
-   Part 1: Pretty-Printing
-   ---------------------------------------------------------------- *)
-
-(** [string_of_op op] returns "+", "-", "*", or "/". *)
 let string_of_op (_o : op) : string =
-  (* EXERCISE: pattern match on Add, Sub, Mul, Div *)
-  failwith "TODO: string_of_op"
+  match _o with
+  | Add -> "+"
+  | Sub -> "-"
+  | Mul -> "*"
+  | Div -> "/"
 [@@warning "-32"]
 
-(** [string_of_expr e] returns a fully parenthesized string.
-    Examples:
-      Num 3            --> "3"
-      Var "x"          --> "x"
-      Neg (Num 5)      --> "(- 5)"
-      BinOp(Add, Num 1, Num 2)  --> "(1 + 2)" *)
-let string_of_expr (_e : expr) : string =
-  (* EXERCISE: pattern match on Num, Var, Neg, BinOp
-     Hint: add [rec] when ready *)
-  failwith "TODO: string_of_expr"
+let rec string_of_expr (_e : expr) : string =
+  match _e with
+  | Num n -> string_of_int n
+  | Var x -> x
+  | Neg e -> "(- " ^ string_of_expr e ^ ")"
+  | BinOp (o, l, r) ->
+    "(" ^ string_of_expr l ^ " " ^ string_of_op o ^ " " ^ string_of_expr r ^ ")"
 
-(* ----------------------------------------------------------------
-   Part 2: Evaluation
-   ---------------------------------------------------------------- *)
+let rec eval (_e : expr) : int option =
+  match _e with
+  | Num n -> Some n
+  | Var _ -> None
+  | Neg e ->
+    (match eval e with
+     | Some n -> Some (-n)
+     | None -> None)
+  | BinOp (o, l, r) ->
+    match eval l, eval r with
+    | Some lv, Some rv ->
+      (match o with
+       | Add -> Some (lv + rv)
+       | Sub -> Some (lv - rv)
+       | Mul -> Some (lv * rv)
+       | Div -> if rv = 0 then None else Some (lv / rv))
+    | _ -> None
 
-(** [eval e] evaluates [e] if it contains no variables.
-    Returns [Some n] on success, [None] if a Var is encountered.
-    Division by zero returns [None]. *)
-let eval (_e : expr) : int option =
-  (* EXERCISE: handle Num, Var, Neg, and BinOp.
-     For BinOp: evaluate both sides; if both are Some, compute.
-     For Div: check for zero denominator.
-     Hint: add [rec] when ready *)
-  failwith "TODO: eval"
-
-(* ----------------------------------------------------------------
-   Provided: Parse helper and main driver
-   ---------------------------------------------------------------- *)
-
-(** [parse_string s] parses the string [s] into an expr. *)
 let parse_string (s : string) : expr =
   let lexbuf = Lexing.from_string s in
   Parser.program Lexer.token lexbuf
 
-(** [test_expr s] parses, prints, and evaluates the expression. *)
 let test_expr (s : string) : unit =
   Printf.printf "Input:  %s\n" s;
   let e = parse_string s in
@@ -70,7 +53,6 @@ let test_expr (s : string) : unit =
 
 let () =
   Printf.printf "=== Exercise 5: Calculator Parser ===\n\n";
-
   test_expr "42";
   test_expr "1 + 2";
   test_expr "3 * 4 + 5";
@@ -78,5 +60,4 @@ let () =
   test_expr "10 - 3 - 2";
   test_expr "-7";
   test_expr "x + 1";
-
   Printf.printf "Done!\n"
